0aee895b7d6c0f351962e7b9ef77f78e17c313eb
Acquiesc <alee38@email.davenport.edu>
2023-05-20 16:25:07 -0400

    Initial Commit

---
+++ DU-Summer-2023-Capstone/Controllers/PagesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DU_Summer_2023_Capstone.Controllers
+{
+    public class PagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Menu()
+        {
+            return View();
+        }
+    }
+}
---
+++ DU-Summer-2023-Capstone/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DU_Summer_2023_Capstone.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
