cef8306b83e44e11a9cbbafaf4fd98094f3266b2
Acquiesc <alee38@email.davenport.edu>
2023-05-25 18:52:25 -0400

    Homepage, Menu
    Initial commits for static homepage/menu UI demos - corresponding with Week 3/Week 4 UI assignments

--- DU-Summer-2023-Capstone/Controllers/PagesController.cs
+++ DU-Summer-2023-Capstone/Controllers/PagesController.cs
@@ -15,5 +15,25 @@ namespace DU_Summer_2023_Capstone.Controllers
             //ie: can search for current user & their most ordered products, then return all products ordered by most purchased
             return View();
         }
+
+        /*
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        
+        public IActionResult storeOrder()
+        {
+            //validate request
+                //if not validated -> return back with errors
+
+            //if validated
+            
+            //store the data
+
+            //return order success page
+        }
+        */
     }
 }
