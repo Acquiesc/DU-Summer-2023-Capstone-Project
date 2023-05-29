29267cdf79a0f37dea5dc36b13197f2e0cc8df4c
Acquiesc <alee38@email.davenport.edu>
2023-05-28 20:50:18 -0400

    Checkout Page
    Finished checkout page UI prototype

--- DU-Summer-2023-Capstone/Controllers/CartController.cs
+++ DU-Summer-2023-Capstone/Controllers/CartController.cs
@@ -16,6 +16,16 @@ namespace DU_Summer_2023_Capstone.Controllers
             return View();
         }
 
+        //this will need to be replaced with checkout POST validation
+        //& a re-direct once orders model has been established
+        public ActionResult OrderDetails()
+        {
+            return View();
+        }
+
+
+        //default methods for reference
+
         // GET: CartController/Details/5
         public ActionResult Details(int id)
         {
@@ -26,21 +36,6 @@ namespace DU_Summer_2023_Capstone.Controllers
         public ActionResult Create()
         {
             return View();
-        }
-
-        // POST: CartController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
 
         // GET: CartController/Edit/5
