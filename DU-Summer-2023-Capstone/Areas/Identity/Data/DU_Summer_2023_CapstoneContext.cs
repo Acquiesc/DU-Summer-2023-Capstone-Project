cdba460a1618468a3b358de39d9eb7bcfb75f865
Acquiesc <alee38@email.davenport.edu>
2023-05-29 19:59:05 -0400

    Auth page styling & bug fixes
    Re-style authentication pages
    
    Find the _partiallogin include & modify to flex row.
    
    Fix cart icon styling on nav bar
    
    Add homepage link to PizzaPete icon in navbar
    
    Adjust font sizes on home page
    
    Fix menu card styling (buttons un-aligned & overflowing)

---
+++ DU-Summer-2023-Capstone/Areas/Identity/Data/DU_Summer_2023_CapstoneContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DU_Summer_2023_Capstone.Data;
+
+public class DU_Summer_2023_CapstoneContext : IdentityDbContext<IdentityUser>
+{
+    public DU_Summer_2023_CapstoneContext(DbContextOptions<DU_Summer_2023_CapstoneContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
